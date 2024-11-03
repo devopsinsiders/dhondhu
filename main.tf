ea7c3905abc20ca3a5cfd2f687949de8ed624a86
DevOps Insiders <124057238+devopsinsiders@users.noreply.github.com>
2024-11-03 22:44:59 +0530

    Revert "Added new Resource Group as per Task#1234"

--- main.tf
+++ main.tf
@@ -7,8 +7,3 @@ resource "azurerm_resource_group" "rg-jackie" {
   name     = "rg-jackie"
   location = "West Europe"
 }
-
-resource "azurerm_resource_group" "rg-rahul" {
-  name     = "rg-rahul"
-  location = "WestUS"
-}
